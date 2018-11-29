d576b846abdbdb3c230818f6dab2a3071517469e
Bobby Jenrow <bobbyjenrow@gmail.com>
2018-11-28 21:45:44 -0500

    Things cleaned up, now to refactor for mobile first

--- site/web/app/themes/cooper-school/front-page.php
+++ site/web/app/themes/cooper-school/front-page.php
@@ -13,34 +13,35 @@ get_header();
 <main class="main">
   <section class="section section--hero">
     <div class="hero">
+      <div class="spacer"></div>
       <div class="container">
         <div class="hero__content">
           <div class="wrapper">
             <h1 class="th">Intentional, Thoughtful, Progressive Education in Charleston, South Carolina</h1>
-            <button type="button" class="btn btn--lg btn--primary">What Are Your Values?</button>
+            <button type="button" class="btn btn--hero btn--primary">What Are Your Values?</button>
           </div>
         </div>
       </div>
     </div>
   </section>
   <section class="section section--about container">
-    <div class="main__content row">
-      <div class="column main-column-left">
+    <div class="row main__content ">
+      <div class="column main__column--left">
         <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/v1543413294/photo-top-left_f5vo4i.jpg" class="img img-homepage-1" />
-        <div class="column-item">
-          <h2 class="t-cta">Our Future</h2>
+        <div class="column__item column__item--left">
+          <h2>Our Future</h2>
           <p>Cultivating the next generation of innovators and explorers, curious scientists and mathematicians, imaginative authors and artists, and engaged change makers.</p>
           <a href="" class="link--important">Our Approach</a>
         </div>
-        <div class="column-item">
-          <h2 class="t-cta">Whole Child Education</h2>
+        <div class="column__item column__item--right">
+          <h2>Whole Child Education</h2>
           <p>Our responsive classrooms value cooperation, assertion, responsibility, empathy, nurturing and gratitude.</p>
           <a href="" class="link--important">Apply Today</a>
         </div>
       </div>
-      <div class="column main-column-right">
-        <div class="column-item--right">
-          <h1 class="t-cta">How Children Learn is as Important as What Children Learn</h1>
+      <div class="column main__column--right">
+        <div class="column__item column__item--right">
+          <h1>How Children Learn is as Important as What Children Learn</h1>
           <button type="button" class="btn btn--primary">Take a Tour</button>
         </div>
         <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/v1543413294/photo-bottom-right_rjxv76.jpg" class="img img-homepage-2" />
@@ -65,21 +66,21 @@ get_header();
     <h1 class="text-section-heading">Upcoming Events</h1>
     <div class="section__events row section__row">
       <div class="event event--first">
-        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/v1543413294/img-galler-left_zx7w3h.jpg" alt="event image" />
+        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/c_scale,w_315/v1543413294/img-galler-left_zx7w3h.jpg" alt="event image" />
         <h5 class="event__date">Nov 5 - 3:00 pm</h5>
         <h2 class="event__title">Event Name</h2>
         <p class="event__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         <a class="link--important">Learn More</a>
       </div>
       <div class="event event--second">
-        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/v1543413292/img-gallery-center_phfu7m.jpg" alt="event image" />
+        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/c_scale,w_315/v1543413292/img-gallery-center_phfu7m.jpg" alt="event image" />
         <h5 class="event__date">Nov 5 - 3:00 pm</h5>
         <h2 class="event__title">Event Name</h2>
         <p class="event__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         <a class="link--important">Learn More</a>
       </div>
       <div class="event event--third">
-        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/v1543413292/img-gallery-right_j5ivjk.jpg" alt="event image" />
+        <img src="https://res.cloudinary.com/ds0f4qyyt/image/upload/c_scale,w_315/v1543413292/img-gallery-right_j5ivjk.jpg" alt="event image" />
         <h5 class="event__date">Nov 5 - 3:00 pm</h5>
         <h2 class="event__title">Event Name</h2>
         <p class="event__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
